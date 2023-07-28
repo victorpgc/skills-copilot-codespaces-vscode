036719550cb34d30e0c02e491693318cf9656656
victorpgc <46329107+victorpgc@users.noreply.github.com>
2023-07-28 11:41:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skills-member");
+    var skillsMemberClose = document.getElementById("skills-member-close");
+    skills.style.display = "block";
+    skillsMember.style.display = "block";
+    skillsMemberClose.style.display = "block";
+}
